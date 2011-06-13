4670979db2656c446fc069297f3f1ac1a11ab7e6
Sumardi Shukor <me@sumardi.net>
2011-06-13 20:20:59 +0800

    Added CalculatorViewController.

--- Calculator/CalculatorViewController.h
+++ Calculator/CalculatorViewController.h
@@ -9,7 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface CalculatorViewController : UIViewController {
-    
+    float total, currentNumber;
+    int currentOperation;
+    UITextField *result;
+    UIButton *equalBtn, *clearBtn;
 }
+
+@property (nonatomic, retain) IBOutlet UITextField *result;
+@property (nonatomic, retain) IBOutlet UIButton *equalBtn;
+@property (nonatomic, retain) IBOutlet UIButton *clearBtn;
+- (IBAction)btnNumberPressed:(id)sender;
+- (IBAction)clearBtnPressed:(id)sender;
+- (IBAction)btnOperatorPressed:(id)sender;
+- (IBAction)equalBtnPressed:(id)sender;
 
 @end
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -10,8 +10,14 @@
 
 @implementation CalculatorViewController
 
+@synthesize result;
+@synthesize equalBtn, clearBtn;
+
 - (void)dealloc
 {
+    [result release]; result = nil;
+    [clearBtn release]; clearBtn = nil;
+    [equalBtn release]; equalBtn = nil;
     [super dealloc];
 }
 
@@ -25,13 +31,55 @@
 
 #pragma mark - View lifecycle
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 }
-*/
+
+#pragma mark - IBAction
+
+- (IBAction)btnNumberPressed:(id)sender 
+{
+    currentNumber = currentNumber * 10 + (float)[sender tag];
+    [result setText:[NSString stringWithFormat:@"%.0f", currentNumber]];
+}
+
+- (IBAction)clearBtnPressed:(id)sender 
+{
+    currentNumber = 0.00;
+    [result setText:@""];
+}
+
+- (IBAction)btnOperatorPressed:(id)sender 
+{
+    total = [[result text] floatValue];
+    currentNumber = 0.00;
+    currentOperation = (int)[sender tag];
+    [result setText:@""];
+}
+
+- (IBAction)equalBtnPressed:(id)sender 
+{
+    switch (currentOperation) {
+        case 1:
+            total = total + currentNumber;
+            break;
+        case 2:
+            total = total - currentNumber;
+            break;
+        case 3:
+            total = total * currentNumber;
+            break;
+        case 4:
+            total = total / currentNumber;
+            break; 
+        default:
+            break;
+    }
+    [result setText:[NSString stringWithFormat:@"%.2f", total]];
+}
 
 - (void)viewDidUnload
 {
